72934084c7f954792dbc91cecc15db71ff43d46a
Koukyosyumei <koukyosyumei@hotmail.com>
2022-07-29 21:05:03 +0900

    update

--- src/ptbi/utils/dataloader.py
+++ src/ptbi/utils/dataloader.py
@@ -215,9 +215,9 @@ def prepare_att_dataloaders(
                 label2cnt[y] += 1
 
             if label2cnt[y] <= 5:
-                x[40 : 40 + blur_strength, :] = 0
-                X_public_list.append(x)
-                # X_public_list.append(cv2.blur(x, (blur_strength, blur_strength)))
+                # x[40 : 40 + blur_strength, :] = 0
+                # X_public_list.append(x)
+                X_public_list.append(cv2.blur(x, (blur_strength, blur_strength)))
                 y_public_list.append(y)
             else:
                 X_private_lists[name_id2client_id[y]].append(x)
