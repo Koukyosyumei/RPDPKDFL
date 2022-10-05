dcb7cebc85f8ee1d33ac07316e311e5df31a5157
Koukyosyumei <koukyosyumei@hotmail.com>
2022-10-06 01:08:35 +0900

    update

--- src/ptbi/attack/tbi_train.py
+++ src/ptbi/attack/tbi_train.py
@@ -4,12 +4,16 @@ import os
 import numpy as np
 import torch
 
-from ..utils.tbi_setup import (setup_our_inv_dataloader,
-                               setup_our_inv_dataloader_from_single_client,
-                               setup_tbi_inv_dataloader)
+from ..utils.tbi_setup import (
+    setup_our_inv_dataloader,
+    setup_our_inv_dataloader_from_single_client,
+    setup_tbi_inv_dataloader,
+)
 from .confidence import get_pi, get_pj
-from .reconstruction import (reconstruct_all_possible_targets,
-                             reconstruct_all_possible_targets_with_pair_logits)
+from .reconstruction import (
+    reconstruct_all_possible_targets,
+    reconstruct_all_possible_targets_with_pair_logits,
+)
 
 
 def train_tbi_inv_model(data, device, inv_model, optimizer, criterion):
@@ -85,7 +89,7 @@ def train_our_inv_model_with_only_priors(
     if gamma != 0:
         output_dim = prior.shape[0]
         target_labels_batch = np.array_split(
-            target_labels, math.ceil(int(len(target_labels) / 64))
+            target_labels, math.ceil(len(target_labels) / 64)
         )
         for label_batch in target_labels_batch:
             optimizer.zero_grad()
@@ -108,7 +112,7 @@ def train_our_inv_model_with_only_priors_paird_logits(
     if gamma != 0:
         output_dim = prior.shape[0]
         target_labels_batch = np.array_split(
-            target_labels, math.ceil(int(len(target_labels) / 64))
+            target_labels, math.ceil(len(target_labels) / 64)
         )
         for label_batch in target_labels_batch:
             optimizer.zero_grad()
