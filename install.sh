e46d9e9e1f088b1072b0b3ede63e01c0e39428c0
Koukyosyumei <koukyosyumei@hotmail.com>
2022-10-30 01:13:27 +0900

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-pip install pybind11
-pip install -U git+https://github.com/Koukyosyumei/AIJack
+pip install -U pip
+pip install "pybind11[global]"
+pip install git+https://github.com/Koukyosyumei/AIJack
+
 pip install -e .
